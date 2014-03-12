3b238c5e74f492e003c01c009ae3faeaf2c0745a
rubeus90 <rubeus90@gmail.com>
2014-03-12 14:32:01 +0100

    Corriger Parametres

--- Parametres.class.php
+++ Parametres.class.php
@@ -1,40 +1,47 @@
 <?php
 class Parametres extends Entities{
-	private $resolutionMiniature,
+	private $id,
+			$resolutionMiniature,
 			$qualiteMiniature,
-			$ordrePhoto,
-			$ordreAlbum,
+			$ordre_photo,
+			$ordre_album,
 			$nombreAffichage;
 
-	public function getResolutionMiniature(){ return $resolutionMiniature;}
-	public function getQualiteMiniature(){ return $qualiteMiniature;}
-	public function getOrdrePhoto(){ return $ordrePhoto;}
-	public function getOrdreAlbum(){ return $ordreAlbum;}
-	public function getNombreAffichage(){ return $nomAffichage;}
+	public function getID(){ return $this->id};
+	public function getResolutionMiniature(){ return $this->resolutionMiniature;}
+	public function getQualiteMiniature(){ return $this->qualiteMiniature;}
+	public function getOrdre_photo(){ return $this->ordre_photo;}
+	public function getOrdre_album(){ return $this->ordre_album;}
+	public function getNombreAffichage(){ return $this->nombreAffichage;}
 
+	public function setId($id){
+		if(is_numeric($id) and !empty($id) and $id>=0){
+			$this->id = $id;
+		}
+	}
 	public function setResolutionMiniature($cote){
 		if(is_numeric($cote) and !empty($cote) and $cote>0){
-			$this->$resolutionMiniature = $cote;
+			$this->resolutionMiniature = $cote;
 		}
 	}
 	public function setQualiteMiniature($qualite){
 		if(is_numeric($qualite) and !empty($qualite) and $qualite>0){
-			$this->$qualiteMiniature = $qualite;
+			$this->qualiteMiniature = $qualite;
 		}
 	}
-	public function setOrdrePhoto($ordre){
+	public function setOrdre_photo($ordre){
 		if(is_string($ordre) and !empty($ordre)){
-			$this->$ordrePhoto = $ordre;
+			$this->ordre_photo = $ordre;
 		}
 	}
-	public function setOrdreAlbum($ordre){
+	public function setOrdre_album($ordre){
 		if(is_string($ordre) and !empty($ordre)){
-			$this->$ordreAlbum = $ordre;
+			$this->ordre_album = $ordre;
 		}
 	}
 	public function setNombreAffichage($nombre){
 		if(is_numeric($nombre) and !empty($nombre)){
-			$this->$nombreAffichage = $nombre;
+			$this->nombreAffichage = $nombre;
 		}
 	}
 }
