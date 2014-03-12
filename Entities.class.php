c34547fc25aa5d4a86aba91985b3d8f044475ef9
7h1b0 <wyrm1992@hotmail.fr>
2014-03-12 11:02:27 +0100

    Class Abstraire Entities

---
+++ Entities.class.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class Entities{
+	public function __construct(array $donnee){
+		if (!empty($donnee))
+			$this->hydrate($donnee);
+	}
+
+	private function hydrate(array $donnee){
+		foreach($donnee as $key => $param){
+			$method = 'set'.ucfirst($key);
+			if(method_exists($this, $method))
+				$this->$method($param);
+		}
+	}
+}
