e60f1e30a37b4790f27cf7f9324702a0ee7a468a
rubeus90 <rubeus90@gmail.com>
2014-03-12 15:10:18 +0100

    En effet, ajouter la classe ParametresManager, c'est encore mieux

---
+++ ParametresManager.class.php
@@ -0,0 +1,48 @@
+<?php
+class ParametresManager{
+	private $bdd;
+
+	public function __construct($bdd){
+		$this->setBdd($bdd);
+	}
+
+	public function setBdd(PDO $db){
+		$this->bdd = $db;
+	}
+
+	public function ajouter(Parametres $param){
+		$query = $this->bdd->prepare('INSERT INTO parametres SET resolutionMiniature=:resolutionMiniature, qualiteMiniature=:qualiteMiniature, ordre_photo=:ordre_photo, ordre_album=:ordre_album, nombreAffichage=:nombreAffichage');
+		
+		$query->bindValue(':resolutionMiniature', $param->getResolutionMiniature());
+		$query->bindValue(':qualiteMiniature', $param->getQualiteMiniature());
+		$query->bindValue(':ordre_photo', $param->getOrdre_photo());
+		$query->bindValue(':ordre_album', $param->getOrdre_album());
+		$query->bindValue(':nombreAffichage', $param->getNombreAffichage());
+
+		$query->execute();
+	}
+
+	public function supprimer(Parametres $param){
+		$query = $this->$bdd->exec('DELETE FROM parametres WHERE id='.$param->id());
+	}
+
+	public function get($id){
+		$query = $this->$bdd->('SELECT resolutionMiniature, qualiteMiniature, ordre_photo, ordre_album, nombreAffichage WHERE id='.$id);
+		$donnees= $query->fetch(PDO::FETCH_ASSOC);	
+
+		return new Parametres($donnees);
+	}
+
+	public function update(Parametres $param){
+		$query = $this->bdd->prepare('UPDATE parametres SET resolutionMiniature=:resolutionMiniature, qualiteMiniature=:qualiteMiniature, ordre_photo=:ordre_photo, ordre_album=:ordre_album, nombreAffichage=:nombreAffichage WHERE id=:id');
+		
+		$query->bindValue(':resolutionMiniature', $param->getResolutionMiniature());
+		$query->bindValue(':qualiteMiniature', $param->getQualiteMiniature());
+		$query->bindValue(':ordre_photo', $param->getOrdre_photo());
+		$query->bindValue(':ordre_album', $param->getOrdre_album());
+		$query->bindValue(':nombreAffichage', $param->getNombreAffichage());
+		$query->bindValue(':id', $param->getId());
+
+		$query->execute();
+	}
+}
