4f5aceb5cea944fca5fe45f85d09505c7c575ff6
KarlakS2 <karlak.s@laposte.net>
2014-03-13 13:31:56 +0100

    Photo et PhotoManager
    Ajout d'un construction pour Photo
    MaJ PhotoManager avec une methode Supprimer et Obtenir

--- PhotoManager.class.php
+++ PhotoManager.class.php
@@ -1,26 +1,70 @@
 <?php
-	class PhotoManager
+	require "Manager.class.php";
+
+	class PhotoManager extends Manager
 	{
-	
-		public __construct()
-		{
 			
-		}
-		
-		public add($photo)
+		public function ajouter($photo)
 		{
-		
+			$requetePrepa = $this->_db->prepare(
+				'INSERT INTO Photo SET
+					titre=:TITRE,
+					description=:DESCRIPTION,
+					url=:URL,
+					urlMiniature=:URLMINIATURE,
+					extension=:EXTENSION,
+					poids=:POIDS,
+					largeur=:LARGEUR,
+					hauteur=:HAUTEUR,
+					date_import=:DATE,
+					acces=:ACCES,
+					album_id:=ALBUMID,
+					note=:NOTE,
+					nombreVotant=:NBVOTANT'
+				);	
+				
+			$requetePrepa->bindValue(':TITRE',$photo->getTitre());
+			$requetePrepa->bindValue(':DESCRIPTION',$photo->getDescription());
+			$requetePrepa->bindValue(':URL',$photo->getUrlMiniature());
+			$requetePrepa->bindValue(':EXTENSION',$photo->getExtension());
+			$requetePrepa->bindValue(':POIDS',$photo->getPoids());
+			$requetePrepa->bindValue(':LARGEUR',$photo->getLargeur());
+			$requetePrepa->bindValue(':HAUTEUR',$photo->getHauteur());
+			$requetePrepa->bindValue(':DATE',$photo->getDateImport());
+			$requetePrepa->bindValue(':ACCES',$photo->getAcces());
+			$requetePrepa->bindValue(':ALBUMID',$photo->getAlbumId());
+			$requetePrepa->bindValue(':NOTE',$photo->getNote());
+			$requetePrepa->bindValue(':NBVOTANT',$photo->getNombreVotant());
+			
+			$requetePrepa->execute();
 		}
 		
-		public get($photo)
+		public function supprimer($photo)
+		{	
+		  $query = $this->$bdd->exec('DELETE FROM parametres WHERE id='.$photo);
+		}
+		
+		public function obtenir($photo)
 		{
-		
+		  $query = $this->$bdd->exec('SELECT FROM parametres WHERE id='.$photo);
+		  $donnees= $query->fetch(\PDO::FETCH_OBJ);
+		  
+		  $objetPhoto = new Photo(
+					$donnees->id,
+					$donnees->titre,
+					$donnees->description,
+					$donnees->url,
+					$donnees->urlMiniature,
+					$donnees->extension,
+					$donnees->poids,
+					$donnees->largeur,
+					$donnees->hauteur,
+					$donnees->dateImport,
+					$donnees->acces,
+					$donnees->albumId,
+					$donnees->note,
+					$donnees->nombreVotant);
+					
 		}
-		
-		public delete($photo)
-		{
-		
-		}
-		
 	}
 ?>
