10e840f3fbfe27dfd58cc8d77f2edeccf4099ce8
7h1b0 <wyrm1992@hotmail.fr>
2014-03-24 14:14:51 +0100

    Affichage des classes chargées

--- Library/autoload.php
+++ Library/autoload.php
@@ -1,9 +1,9 @@
 <?php
 	// Permet de charger automatique les fichiers
 	function autoload($class){
-		//echo '<pre>Autoload : ' . $class;
+		echo '<pre>Autoload : ' . $class;
 		$path =  '../'.str_replace('\\', '/', $class).'.class.php';
-		//echo "\n    =&gt; $path</pre>";
+		echo "\n    =&gt; $path</pre>";
 		require $path;
 	}
 	spl_autoload_register('autoload');
