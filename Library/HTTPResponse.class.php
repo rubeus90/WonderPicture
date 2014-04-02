77639ca9c7eebbef9c0f07fdd8202380773b345d
7h1b0 <wyrm1992@hotmail.fr>
2014-04-02 10:45:54 +0200

    HTTPResponse

--- Library/HTTPResponse.class.php
+++ Library/HTTPResponse.class.php
@@ -12,6 +12,17 @@ class HTTPResponse{
 		exit($page);
 	}
 
+	public function error(){
+		ob_start();
+		require __DIR__.'/../Application/Config/error.html';
+		exit(ob_get_clean());
+	}
+
+	public function redirect404(){
+		header("HTTP/1.0 404 Not Found");
+		exit();
+	}
+
 	public function redirect($location){
 		header('Location: '.$location);
 	}
